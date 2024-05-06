53ae0f6c8c9df0ee632978e942e9e8b7ec1fde58
unknown <ramadangilang930@gmail.com>
2024-05-06 19:17:01 +0700

    Uts_GilangRamadan

---
+++ src/views/uts/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home" >
+
+      <h1>Selamat datang di aplikasi</h1>
+      <h2>Aplikasi ini dibuat oleh</h2>
+      <h3>Npm : 40622100094</h3>
+      <h4>Nama : Gilang Ramadan</h4>
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    components: {
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+    padding: 2rem;
+  }
+  </style>
